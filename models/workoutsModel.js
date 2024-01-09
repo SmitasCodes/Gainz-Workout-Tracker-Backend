460ef02743d81e5ed0051eae386bc8a46843a005
SmitasCodes <airidassmith30@gmail.com>
2024-01-09 18:47:33 +0200

    added schemas

--- models/workoutsModel.js
+++ models/workoutsModel.js
@@ -2,33 +2,35 @@ const mongoose = require("mongoose");
 
 const Workout = mongoose.model(
   "Workout",
-  new mongoose.Schema(
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-      },
-      saved_exercises: {
-        type: [String],
-      },
-      saved_muscle_groups: {
-        type: [String],
-      },
-      workouts_list: [
-        {
-          muscle_groups: String,
-          exercises: [
-            {
-              exercise: { type: String },
-              sets: { type: Number },
-              reps: { type: Number },
-            },
-          ],
-        },
-      ],
+  new mongoose.Schema({
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-  )
+    saved_exercises: {
+      type: [String],
+    },
+    saved_muscle_groups: {
+      type: [String],
+    },
+    Date: {
+      type: Date,
+      required: true,
+    },
+    workouts_list: [
+      {
+        muscle_groups: String,
+        exercises: [
+          {
+            exercise: { type: String },
+            sets: { type: Number },
+            reps: { type: Number },
+          },
+        ],
+      },
+    ],
+  })
 );
 
 module.exports = Workout;
