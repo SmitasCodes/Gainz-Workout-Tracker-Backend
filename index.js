b4760cb4917ccafc75439e036dc953552d9b4325
SmitasCodes <airidassmith30@gmail.com>
2024-01-09 20:14:45 +0200

    added user controller and tested login and sign up features

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const { registerUser, loginUser } = require("./controllers/userController");
 
 connectDB();
 const app = express();
+app.use(express.json());
+
+app.post("/api/users", registerUser);
+app.post("/api/users/login", loginUser);
 
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
